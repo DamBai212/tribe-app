8ff3a6cc651ba1e021dcc4087c6b92aca44d19f0
intuitive-programmer <steven@intuitiveprogrammer.com>
2018-11-14 12:27:11 +0000

    Bootstrap is ready to use

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :courses
-  resources :users
+  resources :users, only: [:index, :edit, :show]
+
+  get '/signup', to: 'users#new', as: 'signup'
+  post '/signup', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
